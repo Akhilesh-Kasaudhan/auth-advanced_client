21fcd30ac5e7357212d0c5eefffde6d68b9906c5
Akhilesh-Kasaudhan <akhil2003kd@gmail.com>
2024-12-28 22:58:46 +0530

    auth-client commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-gray-700 w-full h-screen p-8">
+      <h1 className="text-center text-4xl text-white ">Auth App</h1>
+    </div>
+  );
+};
+
+export default App;
